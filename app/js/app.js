b4240ee5f599e4705ebe9eaa2efeefc62bf29fbc
Jordan Buckley <jordan@bkly.org>
2014-09-14 18:49:15 -0700

    mean and mode working

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 require('angular/angular');
+require('angular-route');
 
-var avgApp = angular.module('avgApp');
+var avgApp = angular.module('avgApp', ['ngRoute']);
 
 // controller(s)
+require('./controllers/avg-controller')(avgApp);
 
 // filter(s)
 
@@ -12,3 +14,13 @@ var avgApp = angular.module('avgApp');
 
 // directive(s)
 
+avgApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/avg-view.html',
+      controller: 'avgController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
